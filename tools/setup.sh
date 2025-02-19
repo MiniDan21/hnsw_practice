171ed8625d5fe236f00b6dedc62abcf43b5faa94
MiniDan21 <molchanov_danilka00@mail.ru>
2025-02-20 02:08:48 +0300

    Fixed setup

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,3 +1,11 @@
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# bash Miniconda3-latest-Linux-x86_64.sh
+# source ~/.bashrc
+# source miniconda3/etc/profile.d/conda.sh
+# conda init zsh
+# source ~/.zshrc
+# conda config --set auto_activate_base true
+
 pip install poetry
 poetry config virtualenvs.path .venv
 poetry config virtualenvs.in-project true
--- tools/start.sh
+++ tools/start.sh
@@ -1,1 +1,1 @@
-poetry run python3 main.py
+poetry run python3 make_index.py --input_dir data --M 10 --ef 30 --thr_num 12
